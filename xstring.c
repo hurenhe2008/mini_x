e14c25f162d86d025ab6d6f5c7736798a66624f6
HURH401-PC\hurh <hurenhe2008@gmail.com>
2017-02-15 16:34:00 +0800

    update xstring [add string operation series function]

--- xstring.c
+++ xstring.c
@@ -30,36 +30,30 @@
 void* xmemset(void *dst, int ch, unsigned n)
 {
     char *d = (char *)dst;
-    char v = ch & 0xFF;  //truncate
+    char v = ch & 0xFF;   /*truncate*/
 
-    if (!d || !n) {
+    if (!d || !n) 
         return nullptr;
-    }
 
-    while (n--) {
+    while (n--) 
         d[n] = v;
-    }
 
     return dst;
 }
-
 
 void* xmemcpy(void *dst, const void *src, unsigned n)
 {
     const char *s = (const char *)src;
     char *d = (char *)dst;
 
-    if (!d || !s || !n) {
+    if (!d || !s || !n) 
         return nullptr;
-    }
 
-    while (n--) {
+    while (n--) 
         d[n] = s[n];
-    }
 
     return dst;
 }
-
 
 void* xmemmove(void *dst, const void *src, unsigned n)
 {
@@ -67,9 +61,8 @@ void* xmemmove(void *dst, const void *src, unsigned n)
     char *d = (char *)dst;
     unsigned i = n;
 
-    if (!d || !s || !n) {
+    if (!d || !s || !n) 
         return nullptr;
-    } 
 
     if (s > d) {
         while (i--) {
@@ -84,19 +77,16 @@ void* xmemmove(void *dst, const void *src, unsigned n)
     }
 
     /*s == d, not handle*/
-
     return dst;
 }
-
 
 int xmemcmp(const void *dst, const void *src, unsigned n)
 {
     const char *d = (const char *)dst;
     const char *s = (const char *)src;
 
-    if (!d || !s || 0 == n) {
+    if (!d || !s || 0 == n) 
         return -1;
-    }
 
     while (n--) {
         if (d[n] != s[n]) {
@@ -107,24 +97,22 @@ int xmemcmp(const void *dst, const void *src, unsigned n)
     return 0;
 }
 
-
 int xmemcpy_s(void *dst, unsigned size, const void *src, unsigned n)
 {
     const char *s = (const char *)src;
     char *d = (char *)dst;
 
-    if (!d || !s || !size || !n || (size < n)) {
+    if (!d || !s || !size || !n) 
         return -1;
-    }
 
-    while (n--) {
+    n = (n > size) ? size : n;  /*if n > size, truncate*/
+
+    while (n--) 
         d[n] = s[n];
-    }
 
     return 0;
 
 }
-
 
 int xmemmove_s(void *dst, unsigned size, const void *src, unsigned n)
 {
@@ -132,13 +120,14 @@ int xmemmove_s(void *dst, unsigned size, const void *src, unsigned n)
     char *d = (char *)dst;
     unsigned i = n;
 
-    if (!d || !s || !size || !n || (size < n)) {
+    if (!d || !s || !size || !n || (size < n)) 
         return -1;
-    }
+
+    n = (n > size) ? size : n;  /*if n > size, truncate*/
 
     if (s > d) {
         while (i--) {
-            d[n - i - 1] = s[n - i - 1];
+            d[n-i-1] = s[n-i-1];
         }
     }
 
@@ -149,6 +138,227 @@ int xmemmove_s(void *dst, unsigned size, const void *src, unsigned n)
     }
 
     /*s == d, not handle*/
+    return 0;
+}
+
+
+
+unsigned xstrlen(const char *src)
+{
+    unsigned cnt = 0;
+
+    if (!src) 
+        return cnt;
+
+    while (*src++) 
+        ++cnt;
+
+    return cnt;
+}
+
+int xstrcmp(const char *src1, const char *src2)
+{
+    while (*src1 && *src2) {
+
+        if (*src2 > *src1) return -1;
+
+        if (*src2 < *src1) return 1;
+
+        ++src1;  ++src2;
+    }
+
+    if (*src2 > *src1) return -1;
+
+    if (*src2 < *src1) return 1;
+
+    return 0;
+}
+
+char* xstrcpy(char *dst, const char *src)
+{
+    char *d = dst;
+
+    if (!dst || !src) 
+        return nullptr;
+
+    while (*src) 
+        *dst++ = *src++;
+
+    *dst = '\0';
+
+    return d;
+}
+
+char* xstrcat(char *dst, const char *src)
+{
+    char *d = dst;
+
+    if (!dst || !src) 
+        return nullptr;
+
+    while (*dst) {
+        dst = dst + 1;
+    }
+
+    while (*src) 
+        *dst++ = *src++;
+
+    *dst = '\0';
+
+    return d;
+}
+
+char* xstrstr(const char *dst, const char *src)
+{
+    const char *s = src, *d = dst;
+    unsigned dlen = 0, slen = 0;
+    int gaplen = 0, i = 0, j = 0;
+
+    if (!dst || !src || !*dst || !*src) 
+        return nullptr;
+
+    while (*s++)  ++slen;
+    while (*d++)  ++dlen;
+    
+    if ((gaplen = dlen - slen) < 0)
+        return nullptr;
+
+    s = src;  
+
+    for (i = 0; i <= gaplen; ++i) {
+
+        d = dst + i;
+
+        for (j = 0; j < slen; ++j) 
+            if (d[j] != s[j]) 
+                break;
+                
+        if (j == slen) return d;
+    }
+
+    return nullptr;
+}
+
+char* xstrrstr(char *dst, const char *src)
+{
+    const char *s = src, *d = dst;
+    unsigned dlen = 0, slen = 0;
+    int gaplen = 0, i = 0, j = 0;
+
+    if (!dst || !src || !*dst || !*src) 
+        return nullptr;
+
+    while (*s++)  ++slen;
+    while (*d++)  ++dlen;
+
+    if ((gaplen = dlen - slen) < 0) 
+        return nullptr;
+
+    s = src;
+
+    for (i = gaplen; i >= 0; --i) {
+
+        d = dst + i;
+
+        for (j = 0; j < slen; ++j)
+            if (d[j] != s[j]) 
+                break;
+
+        if (j == slen) return d;
+    }
+
+    return nullptr;
+}
+
+char* xstrchr(const char *dst, char val)
+{
+    if (!dst)  return nullptr;
+
+    while (*dst) {
+        if (val == *dst) 
+            return dst;
+        ++dst;
+    }
+
+    if (val == *dst)  /*'\0'*/
+        return dst;
+
+    return nullptr;
+}
+
+char* xstrrchr(const char *dst, char val)
+{
+    unsigned dlen = 0;
+    const char *d = dst;
+    
+    while (*dst++)   ++dlen;
+
+    while (dst >= d) {
+        if (val == *dst) return dst; /*include '\0'*/ 
+        --dst;
+    }
+    
+    return nullptr;
+}
+
+char* xstrcpy_s(char *dst, unsigned size, const char *src)
+{
+    unsigned i = 0;
+
+    if (!dst || !src || !size || !*src)  
+        return nullptr;
+
+    while (*src && i < size) 
+        dst[i++] = *src++;
+
+    dst[i] = '\0';   /*i == size, copy and truncate*/
+
+    return dst;
+}
+
+char* xstrcat_s(char *dst, unsigned size, const char *src)
+{
+    char *d = dst;
+    unsigned i = 0, len = 0, left = 0;
+
+    if (!dst || !src || !size) 
+        return nullptr;
+
+    while (*dst++)  ++len;
+
+    if ((left = size - len) <= 0) 
+        return nullptr;
+
+    i = 0;
+    while (*src && i < left) 
+        dst[i++] = *src++;
+
+    *dst = '\0';
+
+    return d;
+}
+
+int xstrcmp_s(const char *src1, const char *src2, unsigned n)
+{
+    unsigned i = 0;
+
+    if (!src1 || !src2 || !n) {
+        return -2;
+    }
+
+    for (i = 0; i < n; ++i) {
+        if (src2[i] > src1[i]) {
+            return -1;
+        }
+
+        if (src2[i] < src1[i]) {
+            return 1;
+        }
+
+        if (0 == src2[i]) { /*src1[i] == src2[i]: '\0'*/
+            break;
+        }
+    }
 
     return 0;
 }
--- xstring.h
+++ xstring.h
@@ -4,7 +4,8 @@
 *
 *   date:    2017/2/10
 *
-*   declare: accomplish memory series operation function.
+*   declare: accomplish memory series operation function and
+*            string series operation function.
 *
 ********************************************************************/
 
@@ -16,6 +17,8 @@
 #else
     #define CAPI
 #endif 
+
+//================================ memory.h ===================================
 
 /***
 * @function  
@@ -99,6 +102,157 @@ CAPI int xmemcpy_s(void *dst, unsigned size, const void *src, unsigned n);
 *          otherwise return -1
 */
 CAPI int xmemmove_s(void *dst, unsigned size, const void *src, unsigned n);
+
+
+
+//================================ string.h ===================================
+
+
+/***
+* @function
+*          xstrlen()
+* @param
+*          src - the source buffer
+* @return
+*          return src buffer length       
+*/
+CAPI unsigned xstrlen(const char *src);
+
+
+/***
+* @function
+*          xstrcmp()
+* @param
+*          dst - the destination buffer
+*          src - the source buffer
+*          n -   move count
+* @return
+*          src1 < src2, return -1
+*          src1 = src2, return 0
+*          src1 > src2, return 1        
+*/
+CAPI int xstrcmp(const char *src1, const char *src2);
+
+
+/***
+* @function
+*          xstrcpy()
+* @param
+*          dst - the destination buffer
+*          src - the source buffer
+* @return
+*          if success, return dst addr
+*          if fail, return nullptr
+*/
+CAPI char* xstrcpy(char *dst, const char *src);
+
+
+/***
+* @function
+*          xstrcat()
+* @param
+*          dst - the destination buffer
+*          src - the source buffer
+* @return
+*          if success, return dst addr
+*          if fail, return nullptr
+*/
+CAPI char* xstrcat(char *dst, const char *src);
+
+
+/***
+* @function
+*          xstrstr()
+* @param
+*          dst - the destination buffer
+*          src - the source buffer
+* @return
+*          if success, return the address with src first found in dst
+*          if fail, return nullptr
+*/
+CAPI char* xstrstr(const char *dst, const char *src);
+
+
+/***
+* @function
+*          xstrrstr()
+* @param
+*          dst - the destination buffer
+*          src - the source buffer
+* @return
+*          if success, return the address with src last found in dst
+*          if fail, return nullptr
+*/
+CAPI char* xstrrstr(const char *dst, const char *src);
+
+
+/***
+* @function
+*          xstrchr()
+* @param
+*          dst - the destination buffer
+*          val - look for character
+* @return
+*          if success, return the first find val 's address.
+*          if fail, return nullptr
+*/
+CAPI char* xstrchr(const char *dst, char val);
+
+
+/***
+* @function
+*          xstrrchr()
+* @param
+*          dst - the destination buffer
+*          val - look for character
+* @return
+*          if success, return the last find val 's address.
+*          if fail, return nullptr
+*/
+CAPI char* xstrrchr(char *dst, char val);
+
+
+/***
+* @function
+*          xstrcpy_s()
+* @param
+*          dst  - the destination buffer
+*          size - the destination buffer size
+*          src  - the source buffer
+* @return
+*          if success, return dst addr
+*          if fail, return nullptr
+*/
+CAPI char* xstrcpy_s(char *dst, unsigned size, const char *src);
+
+
+/***
+* @function
+*          xstrcat_s()
+* @param
+*          dst  - the destination buffer
+*          size - the destination buffer size
+*          src  - the source buffer
+* @return
+*          if success, return dst addr
+*          if fail, return nullptr
+*/
+CAPI char* xstrcat_s(char *dst, unsigned size, const char *src);
+
+
+/***
+* @function
+*          xstrcmp_s()
+* @param
+*          src1 - the src1 buffer
+*          src2 - the src2 buffer
+*          n    - compare number
+* @return
+*          src1 < src2, return -1
+*          src1 = src2, return 0
+*          src1 > src2, return 1
+*/
+CAPI int xstrcmp_s(const char *src1, const char *src2, unsigned n);
 
 
 #endif //_XSTRING_H_
